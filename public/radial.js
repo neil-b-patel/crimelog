eac67c38433376f644dc63d996087e344de2e39e
nepatel <nepatel@davidson.edu>
2020-06-18 22:38:19 -0400

    change upper padding on radial

--- public/radial.js
+++ public/radial.js
@@ -29,7 +29,7 @@ function scaleRadial() {
   }
 
 function radialChart() {
-  let margin = { top: 100, right: 10, bottom: 10, left: 10},
+  let margin = { top: 10, right: 10, bottom: 10, left: 10},
     width = 1105 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom,
     innerRadius = 150,
